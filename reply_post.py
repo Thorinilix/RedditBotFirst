12aa8388df5f3a614d66523bb85bfd5c5ccaac6e
Thorinilix <thorinilix@gmail.com>
2015-05-25 02:27:06 -0400

    somethingelser

--- reply_post.py
+++ reply_post.py
@@ -1,11 +1,37 @@
 import praw
+import pdb
+import re
+import os
+from config_bot import *
+
+if not os.path.isfile("config_bot.py"):
+	print "You must create a config file with your username and password."
+	print "Please see config_skel.py"
+	exit(1)
 
 user_agent = ("PyEng Bot 0.1")
 r = praw.Reddit(user_agent = user_agent)
-subreddit = r.get_subreddit("learnpython")
 
+r.login(REDDIT_USERNAME, REDDIT_PASS)
+
+if not os.path.isfile("posts_replied_to.txt"):
+	posts_replied_to = []
+else:
+	with open("posts_replied_to.txt", "r") as f:
+		posts_replied_to = f.read()
+		posts_replied_to = posts_replied_to.split("\n")
+		posts_replied_to = filter(None, posts_replied_to)
+
+subreddit = r.get_subreddit('pythonforengineers')
 for submission in subreddit.get_hot(limit = 5):
-	print "Title: ", submission.title
-	print "Text: ", submission.selftext
-	print "Score: ", submission.score
-	print "-------------------------------\n"
+	print submission.title
+	
+	if submission.id not in posts_replied_to:
+		if re.search("i love python", submission.title, re.IGNORECASE):
+			submission.add_comment("Nigerian scammer bot says: It's all about the Bass (and Python)")
+			print "Bot replying to : ", submission.title
+			posts_replied_to.append(submission.id)
+
+with open("posts_replied_to.txt", "w") as f:
+	for post_id in posts_replied_to:
+		f.write(post_id + "\n")
